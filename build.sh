0ee85938d38b10a677225a46bc75b95616dc32c8
danielcm585 <74773920+danielcm585@users.noreply.github.com>
2021-07-09 11:05:46 +0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+php add.php
+php apply.php
+php cancel.php
+php connection.php
+php doctor.php
+php function.php
+php login.php
+php logout.php
+php patient.php
+php register.php
+php remove.php
