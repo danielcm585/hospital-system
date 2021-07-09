407d03ae107fe08f1c9454e9d68bd619d617ac0f
danielcm585 <74773920+danielcm585@users.noreply.github.com>
2021-07-09 11:07:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-php add.php
-php apply.php
-php cancel.php
-php connection.php
-php doctor.php
-php function.php
-php login.php
-php logout.php
-php patient.php
-php register.php
-php remove.php
+php /api/add.php
+php /api/apply.php
+php /api/cancel.php
+php /api/connection.php
+php /api/doctor.php
+php /api/function.php
+php /api/login.php
+php /api/logout.php
+php /api/patient.php
+php /api/register.php
+php /api/remove.php
